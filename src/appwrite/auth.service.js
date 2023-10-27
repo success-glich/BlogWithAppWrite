b6ab32c2e271694ffdb088e58fea9d717f871a51
Ghorasaini Success <imsaphal21@gmail.com>
2023-10-27 15:53:15 +0545

    add appwrite auth service

---
+++ src/appwrite/auth.service.js
@@ -0,0 +1,62 @@
+import config from "../config/config"
+import { Client, Account, ID } from "appwrite";
+
+class AuthService {
+    client = new Client();
+    account;
+    constructor() {
+        this.client.setEndpoint(config.appwriteUrl).setProject(config.appwriteProjectId);
+        this.account = new Account(this.client);
+    }
+    async createAccount({ email, password, name }) {
+
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            if (userAccount) {
+                //call another method to login
+                return this.login({ email, password })
+            } else {
+                return userAccount
+            }
+        } catch (err) {
+            throw err;
+
+        }
+    }
+    async login({ email, password }) {
+
+        try {
+            return await this.account.createEmailSession(email, password);
+
+        } catch (err) {
+
+        }
+    }
+
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
+        } catch (err) {
+            console.log("AppWrite Service:: getCurrentUser :: User", err)
+        }
+
+        return null;
+
+    }
+    async logout() {
+        try {
+            await this.account.deleteSessions();
+
+        } catch (err) {
+            console.log("AppWrite Service::logout :: error", err)
+
+
+        }
+
+
+    }
+}
+
+const authService = new AuthService();
+
+export default authService;
