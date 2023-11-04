b3b89752d10a2653fb53556135e0e0fed051f594
Ghorasaini Success <imsaphal21@gmail.com>
2023-11-04 16:17:39 +0545

    signup, login, authLayout created

--- src/components/index.js
+++ src/components/index.js
@@ -3,10 +3,14 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import Button from "./Button";
+import Input from "./Input";
 export {
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    Button,
+    Input
 }
